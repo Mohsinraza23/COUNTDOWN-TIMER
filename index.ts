153656330178a874d6ffa13b73e226a3cff0fb01
Mohsinraza23 <github.com/Mohsinraza23>
2024-04-13 16:24:26 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,40 @@
+import { differenceInSeconds, min } from "date-fns";
+import inquirer from "inquirer";
+
+
+const res = await inquirer.prompt({
+    type: "number",
+    name: "userinput",
+    message: "please enter the amount of second",
+    validate: (input) => {
+        if (isNaN(input)){
+            return "Please enter valid number";
+        }else if (input > 60) {
+            return "seconds must be in 60";
+        }else {
+            return true;
+        }
+    }
+});
+
+let input = res.userinput
+
+function startTime(val:number){
+    const intTime = new Date().setSeconds(new Date().getSeconds() + val);
+    const intervalTime = new Date(intTime);
+    setInterval(() => {
+        const currentTime = new Date()
+        const timeDiff = differenceInSeconds(intervalTime , currentTime);
+        if (timeDiff <= 0) {
+            console.log(`Timer has expired`);
+            process.exit();
+
+        }
+        const min = Math.floor((timeDiff % (3600 * 24)) / 3600)
+        const sec = Math.floor(timeDiff % 60);
+        console.log(`${min.toString().padStart(2 , "0")} : ${sec.toString().padStart(2 , "0")}`);
+}, 1000);
+}
+
+startTime(input);
+
